1d69805112f38e20526d848654b2ccb2435dfab7
hoainam03052003 <22h1330011@ut.edu.vn>
2026-09-18 20:22:05 +0700

    file database

---
+++ CODE/database/seed.sql
@@ -0,0 +1,7 @@
+USE tttn_mis_04;
+
+INSERT IGNORE INTO event_types (event_type_id,type_name,description) VALUES
+(1,'Training','Khóa đào tạo nội bộ'),
+(2,'Workshop','Hội thảo/workshop'),
+(3,'Seminar','Hội thảo chuyên đề'),
+(4,'Internal Event','Sự kiện nội bộ');
